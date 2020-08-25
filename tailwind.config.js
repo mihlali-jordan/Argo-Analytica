b16c2b2939ecad4f2b24ab5986098548c515df27
Mihlali Jordan <biz@jordanmihlali.dev>
2020-08-25 12:11:54 +0200

    Finished coding UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
         argo: {
           primary: '#3BFF95',
           secondary: '#2DF759',
-          dark: '#282C34'
+          dark: '#282C34',
+          grey: '#616770',
+          darkGrey: '#323741'
         }
       },
       fontFamily: {
