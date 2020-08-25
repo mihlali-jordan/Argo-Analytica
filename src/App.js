1fb1e06483e7bed079d88a6ad4092da6ed0721a7
Mihlali Jordan <biz@jordanmihlali.dev>
2020-08-25 09:45:17 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import argoLogo from './argo-logo.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={argoLogo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
