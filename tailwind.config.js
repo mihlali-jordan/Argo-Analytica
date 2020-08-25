a14544fb34f5ee6c27ea4826dcded67ae46caac9
Mihlali Jordan <biz@jordanmihlali.dev>
2020-08-25 10:17:31 +0200

    Installed and configured Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        argo: {
+          primary: '#3BFF95',
+          secondary: '#2DF759',
+          dark: '#282C34'
+        }
+      },
+      fontFamily: {
+        'montserrat': ['montserrat', 'sans-serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
