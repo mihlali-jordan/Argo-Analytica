d44cbcbda1b0e3cef4a9978257ff01f9fce2047b
Mihlali Jordan <biz@jordanmihlali.dev>
2020-08-26 09:59:07 +0200

    Added logic to display correct emojis

--- src/components/Sentiment.js
+++ src/components/Sentiment.js
@@ -12,19 +12,25 @@ export const Sentiment = ({ mood }) => {
 	
 	const totalPosts = sadSentimentCount + happySentimentCount + mehSentimentCount
 
-	
+	// svg paths for icons
+
+	let svgPath = ""
 	let sentimentPer = 0
 	let color = "green"
 
+	// Using prop to determine the appropriate colors to display and set the appropriate sentiment percentage
 	if(mood === "sad") {
 		color="red"
 		sentimentPer = Math.round((sadSentimentCount/totalPosts)*100)
+		svgPath = "M4.285 12.433a.5.5 0 00.683-.183A3.498 3.498 0 018 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 00.866-.5A4.498 4.498 0 008 9.5a4.5 4.5 0 00-3.898 2.25.5.5 0 00.183.683z"
 	} else if(mood === "meh"){
 		color="yellow"
 		sentimentPer = Math.round((mehSentimentCount/totalPosts)*100)
+		svgPath = "M4 10.5a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5z"
 	} else {
 		color="green"
 		sentimentPer = Math.round((happySentimentCount/totalPosts)*100)
+		svgPath = "M4.285 9.567a.5.5 0 01.683.183A3.498 3.498 0 008 11.5a3.498 3.498 0 003.032-1.75.5.5 0 11.866.5A4.498 4.498 0 018 12.5a4.498 4.498 0 01-3.898-2.25.5.5 0 01.183-.683z"
 	}
 
 
@@ -53,7 +59,7 @@ export const Sentiment = ({ mood }) => {
 					/>
 					<path
 						fillRule='evenodd'
-						d='M4 10.5a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5z'
+						d={svgPath}
 					/>
 					<path d='M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z' />
 				</g>
