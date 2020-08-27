cb2243107e2c9351857a914a44f0c50791dab8f3
Mihlali Jordan <biz@jordanmihlali.dev>
2020-08-27 06:08:21 +0200

    Fixed prop in Sentiment component

--- src/components/Sentiment.js
+++ src/components/Sentiment.js
@@ -5,10 +5,11 @@ import {Posts} from '../data/Posts'
 export const Sentiment = (props) => {
 	
 	// Math operations to calculate overall sentiment percentages
+	const mood = props.mood
 	const posts = Posts
-	const sadSentimentCount = posts.filter(post => post.props.mood === 0).length
-    const happySentimentCount = posts.filter(post =>  post.props.mood === 2).length
-	const mehSentimentCount = posts.filter(post => post.props.mood === 1).length
+	const sadSentimentCount = posts.filter(post => post.mood === 0).length
+    const happySentimentCount = posts.filter(post =>  post.mood === 2).length
+	const mehSentimentCount = posts.filter(post => post.mood === 1).length
 	
 	const totalPosts = sadSentimentCount + happySentimentCount + mehSentimentCount
 
@@ -19,11 +20,11 @@ export const Sentiment = (props) => {
 	let color = "green"
 
 	// Using prop to determine the appropriate colors to display and set the appropriate sentiment percentage
-	if(props.mood === "sad") {
+	if(mood === "sad") {
 		color="red"
 		sentimentPer = Math.round((sadSentimentCount/totalPosts)*100)
 		svgPath = "M4.285 12.433a.5.5 0 00.683-.183A3.498 3.498 0 018 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 00.866-.5A4.498 4.498 0 008 9.5a4.5 4.5 0 00-3.898 2.25.5.5 0 00.183.683z"
-	} else if(props.mood === "meh"){
+	} else if(mood === "meh"){
 		color="yellow"
 		sentimentPer = Math.round((mehSentimentCount/totalPosts)*100)
 		svgPath = "M4 10.5a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5z"
@@ -66,7 +67,7 @@ export const Sentiment = (props) => {
 			</svg>
 			<div className='flex flex-col justify-center items-center'>
 				<div className={sentimentState.sentimentTag}>
-					<h1 className='flex m-auto justify-center'>{props.mood}</h1>
+					<h1 className='flex m-auto justify-center'>{mood}</h1>
 				</div>
 				<h1 className={sentimentState.sentimentPercentage}>{`${sentimentPer}%`}</h1>
 			</div>
